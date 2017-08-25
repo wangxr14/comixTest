316cf735c1a63cfe7bf9d2e3b725e5467fdb452c
wangxr14 <wangxr1108@126.com>
2017-08-25 12:22:59 -0500

    Runserver

--- app/views.py
+++ app/views.py
@@ -4,9 +4,9 @@ from app import app
 
 @app.route("/")
 def index():
-    #return "Hello World"
+    return "Hello World"
 	#return render_template("test.html")
-	return render_template("index.html")
+	#return render_template("index.html")
 	
 @app.route("/xkcd/")
 def xkcd():
